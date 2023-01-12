f5568aa2704c84bb2d9268e5281dc8c5077745be
Rodrigo Flores <rfloresa1900@alumno.ipn.mx>
2023-01-12 15:16:04 -0600

    Definiendo pruebas

--- Python project 1/main.py
+++ Python project 1/main.py
@@ -1,16 +1,27 @@
-import pyttsx3, PyPDF2
-print("First. Make you shure that pdf that would be converted on mp3 are on this folder")
-# nameofpdf=input("Hey! can you show me what's the name of your book? pls end with '.pdf' => ")
-# if nameofpdf.isalnum() and nameofpdf.endswith('.pdf'):
-#     pdfreader=PyPDF2.PdfFileReader(open(nameofpdf, 'rb'))
-# else:
-#     print("you don't pay attention dude")
-pdfreader=PyPDF2.PdfReader(open('Unidad3.pdf', 'rb'))
+import pyttsx3
+from PyPDF2 import PdfReader
+print("First. Make you shure that pdf that would be converted on mp3 are on this current folder")
+nameofpdf=input("Hey! can you show me what's the name of your book? pls end with '.pdf' => ")
+reader=''
+if nameofpdf.endswith('.pdf'):
+    nameofpdf=nameofpdf
+else:
+    nameofpdf=nameofpdf+".pdf"
+reader = PdfReader(open(nameofpdf, "rb"))#open pdf 
+number_of_pages = len(reader.pages)
+print(reader)
+print(number_of_pages)
+page = reader.pages[2]#para cada numero de páginas
+text = page.extract_text(0)
+clean_text=text.strip().replace('\n', ' ')
+print(clean_text)
 speaker=pyttsx3.init()
-for page_num in range(pdfreader.numPages):
-    text=pdfreader.getPage(page_num).extractText()
-    clean_text=text.strip().replace('\n', ' ')
-    print(clean_text)
 speaker.save_to_file(clean_text, 'story.mp3')
 speaker.runAndWait()
 speaker.stop()
+#------------------------------------------------------
+# for page_num in number_of_pages:
+#     text=reader.getPage(page_num).extractText()
+#     clean_text=text.strip().replace('\n', ' ')
+#     print(clean_text)
+#-----------------------------------------------------------
